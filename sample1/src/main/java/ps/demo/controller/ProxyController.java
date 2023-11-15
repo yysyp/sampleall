1a70a5ec4ac95ed6a8d25327f1b6804416f0dcb6
yysyp <yysyp@foxmail.com>
2023-11-15 15:52:42 +0800

    refine the proxycontroller

--- sample1/src/main/java/ps/demo/controller/ProxyController.java
+++ sample1/src/main/java/ps/demo/controller/ProxyController.java
@@ -1,21 +1,25 @@
 package ps.demo.controller;
 
+import com.google.common.html.HtmlEscapers;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import ps.demo.common.MyRestTemplateUtil;
 
-@RestController(value = "/proxy")
+@Slf4j
+@RestController
+@RequestMapping("/proxy")
 public class ProxyController {
 
     @ResponseBody
-    @GetMapping("/bing")
-    public String proxy() {
-        String response = MyRestTemplateUtil.getInstance().getForString("http://www.bing.com/");
-        return StringUtils.abbreviate(response, 80);
+    @GetMapping("/call")
+    public String proxy(
+            @RequestParam(value = "url", defaultValue = "www.bing.com", required = false) String host,
+            @RequestParam(value = "port", defaultValue = "80", required = false) String port) {
+        log.info("proxy port={}", port);
+        String response = MyRestTemplateUtil.getInstance().getForString("http://"+host+":"+port+"/");
+        return HtmlEscapers.htmlEscaper().escape(StringUtils.abbreviate(response, 80));
     }
 }
