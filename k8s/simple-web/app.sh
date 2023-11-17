d757d90e2632862c073c47b007b636608cdcbaa6
yysyp <yysyp@foxmail.com>
2023-11-17 21:52:07 +0800

    add simple web

---
+++ k8s/simple-web/app.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+##sed -i 's/\r$//' *.sh
+set -o nounset
+#set -o errexit
+
+echo '-----------------Initializing...-----------------'
+imgName=nginx
+#ver="v$(date +"%Y%m%d%H%M")"
+ver=latest
+appName=myapp4
+ns="nsaaa"
+containerPort=80
+nodePort=30088
+
+echo '-----------------Kubernetes deploy...-----------------'
+#Login if required...
+sed "s/imageNamePlaceHolder:versionPlaceHolder/$imgName:$ver/g" app.yaml > $appName.yaml.output
+sed -i "s/appNamePlaceHolder/$appName/g" $appName.yaml.output
+sed -i "s/namespacePlaceHolder/$ns/g" $appName.yaml.output
+sed -i "s/containerPortPlaceHolder/$containerPort/g" $appName.yaml.output
+sed -i "s/nodePortPlaceHolder/$nodePort/g" $appName.yaml.output
+
+#kubectl delete -f $appName.yaml.output
+kubectl delete ns $ns
+
+#if [ $? -ne 0 ]; then exit 1; fi
+kubectl apply -f $appName.yaml.output
+if [ $? -ne 0 ]; then exit 1; fi
+
+echo "app deployed to -n $ns"
